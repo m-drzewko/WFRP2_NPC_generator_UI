5e9b31e6fc4191bbb62ef8f3d9032ea4d9331372
mdrzewek <m.m.drzewek@gmail.com>
2023-08-20 13:08:40 +0200

    added npc-list component

---
+++ src/app/npc/npc-list/npc-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NpcListComponent } from './npc-list.component';
+
+describe('NpcListComponent', () => {
+  let component: NpcListComponent;
+  let fixture: ComponentFixture<NpcListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NpcListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NpcListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/npc/npc-list/npc-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-npc-list',
+  templateUrl: './npc-list.component.html',
+  styleUrls: ['./npc-list.component.css']
+})
+export class NpcListComponent {
+
+}
