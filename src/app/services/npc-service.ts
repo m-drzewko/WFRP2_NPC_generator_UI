dc3eedcedcc87363fc0fcbf886761e7d8245ca53
mdrzewek <m.m.drzewek@gmail.com>
2023-08-20 18:02:43 +0200

    initial implementation of fetching paged NPC lists

--- src/app/services/npc-service.ts
+++ src/app/services/npc-service.ts
@@ -6,16 +6,22 @@ import { SingleResponseObject } from "../shared/response/single-response-object"
 import { HOST } from "../shared/utils";
 import { Npc } from "../shared/model/npc";
 import { AuthService } from "./auth.service";
+import { ListResponseObject } from "../shared/response/list-response-object";
+import { PagedResponseObject } from "../shared/response/paged-response-object";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class NpcService {
 
-	private getAllRacesUrl = HOST + 'npc/generate';
+	private generateNpcUrl = HOST + 'npc/generate';
 	private saveNpcUrl = HOST + 'user/auth/npc/save';
+	private pageOfNpcsUrl = HOST + 'auth/npc/getall';
 
 	private headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+	private pageOfNpcs : Npc[]= [];
+	private totalPagesOfNpcs = 0;
 
 	constructor(private httpClient: HttpClient,
 		private authService: AuthService,
@@ -26,7 +32,7 @@ export class NpcService {
 		let headers = new HttpHeaders()
 			.set('Accept-Language', this.translate.currentLang);
 		
-		const url: string = this.getAllRacesUrl + "?race=" + race + "&gender=" + gender;
+		const url: string = this.generateNpcUrl + "?race=" + race + "&gender=" + gender;
 
 		//TODO: change the request type to GET
 
@@ -40,8 +46,6 @@ export class NpcService {
 
 		headers = headers.set('Authorization', this.authService.provideToken());
 
-		console.log(headers);
-
 		this.httpClient.post<SingleResponseObject>(this.saveNpcUrl, npc, {"headers": headers}).subscribe(
 			(data) => {
 				console.log('Post sent: ', data);
@@ -50,7 +54,30 @@ export class NpcService {
 				}
 			}, error => {
 				console.log(error);
+				// TODO implement displaying error dialog
 			}
 		);
+	}
+
+	getPageOfNpcs(page?: number) {
+
+		if (page === null) {
+			page = 0;
+		}
+
+		let headers = this.headers;
+		headers.set('Authorization', this.authService.provideToken());
+
+		let url = this.pageOfNpcsUrl + "?page=" + page;
+
+		this.httpClient.get<PagedResponseObject>(url, {"headers": headers}).subscribe(
+			(data) => {
+				this.pageOfNpcs = data.object;
+				this.totalPagesOfNpcs = data.pages;
+			}, error => {
+				console.log(error);
+				// TODO implement displaying error dialog
+			}
+		)
 	}
 }
